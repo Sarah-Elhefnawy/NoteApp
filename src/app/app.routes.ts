dc792544b45d66faa1922a3169be2c1cbdfa2aae
Sarah <sarahelhefnawy0@gmail.com>
2025-09-30 23:49:48 +0300

    main.server.ts Error+Install Packages+Implement Layouts+Routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,27 @@
 import { Routes } from '@angular/router';
+import { AuthLayoutComponent } from './core/layouts/auth-layout/auth-layout.component';
+import { LoginComponent } from './core/components/login/login.component';
+import { RegisterComponent } from './core/components/register/register.component';
+import { MainLayoutComponent } from './core/layouts/main-layout/main-layout.component';
+import { HomeComponent } from './features/home/home.component';
+import { NoteDetailsComponent } from './features/note-details/note-details.component';
+import { NotFoundComponent } from './features/not-found/not-found.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: 'home', pathMatch: 'full' },
+
+    {
+        path: '', component: AuthLayoutComponent, children: [
+            { path: 'logIn', component: LoginComponent, title: 'Login' },
+            { path: 'register', component: RegisterComponent, title: 'Register' }
+        ]
+    },
+
+    {
+        path: '', component: MainLayoutComponent, children: [
+            { path: 'home', component: HomeComponent, title: 'Home' },
+            { path: 'noteDetails', component: NoteDetailsComponent, title: 'Note Details' }
+        ]
+    },
+    { path: '**', component: NotFoundComponent, title: '404' }
+];
